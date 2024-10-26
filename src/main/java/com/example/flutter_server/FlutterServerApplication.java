326a1fb68fc03c0008afb2a51178dfade6a480ba
seokgenius <sjk@forbiz.co.kr>
2024-10-26 16:35:06 +0900

    first commit

---
+++ src/main/java/com/example/flutter_server/FlutterServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flutter_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterServerApplication.class, args);
+	}
+
+}
